41d1a4f6885decd6d711eacc39e3572a3832c22e
Ho2bes <8696@holbertonstudents.com>
2025-04-25 14:08:02 +0200

    change css

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,23 +1,27 @@
 import './App.css';
 import logo from './assets/holberton-logo.jpg';
 import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="Holberton logo" />
-        <h1>School dashboard</h1>
-      </header>
+    <>
+      <Notifications />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="Holberton logo" />
+          <h1>School dashboard</h1>
+        </header>
 
-      <main className="App-body">
-        <p>Login to access the full dashboard</p>
-      </main>
+        <main className="App-body">
+          <p>Login to access the full dashboard</p>
+        </main>
 
-      <footer className="App-footer">
-        <p><i>Copyright {getCurrentYear()} - {getFooterCopy(true)}</i></p>
-      </footer>
-    </div>
+        <footer className="App-footer">
+          <p><i>Copyright {getCurrentYear()} - {getFooterCopy(false)}</i></p>
+        </footer>
+      </div>
+    </>
   );
 }
 
