4bad1be44b0c39b0b0c23489f2be573f3a2e4fa3
Ho2bes <8696@holbertonstudents.com>
2025-04-28 13:44:26 +0200

    Updates

--- Webpack/README.md
+++
@@ -1,3 +0,0 @@
-webpack
-# Webpack
-Webpack is a static module bundler for modern JavaScript applications. It takes modules with dependencies and generates static assets representing those modules.
--- react_intro/task_1/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-// task_1/dashboard/src/utils.js
-
-export function getCurrentYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
