ce5fdaf6f8e62676099434bac02447f741b62d8a
Ho2bes <8696@holbertonstudents.com>
2025-04-28 11:59:47 +0200

    Updates

---
+++ Webpack/task_3/modules/body/body.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
+
+let count = 0;
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('body').append("<button id='startBtn'>Click here to get started</button>");
+$('body').append("<p id='count'></p>");
+
+$('#startBtn').on('click', _.debounce(updateCounter, 500));
---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import '../header/header.css';
+
+console.log('Init header');
+$('body').prepend('<div id="logo"></div>');
+$('body').prepend('<h1>Holberton Dashboard</h1>');
