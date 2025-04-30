c11eaa6c765724c63305af8eedfffc5456849ba0
Ho2bes <8696@holbertonstudents.com>
2025-04-30 15:50:31 +0200

    add task 1

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Fragment } from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <Header />
+      <div className="App-body">
+        <Login />
+      </div>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
